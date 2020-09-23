83ffe7fb6b134c436e2732356292aa28a0845dd9
zxd <113@163.com>
2020-09-23 15:29:38 +0800

    模板的基本使用

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 # 导入HttpResponse模块
 from django.http import HttpResponse
+from django.http import HttpRequest
+
 
 # Create your views here.
 
 # 定义视图函数
 def index(request):
-
-    return HttpResponse('ok!')
+    context={
+        'name':'詹姆斯'
+    }
+    return render(request,'index.html', context=context)
